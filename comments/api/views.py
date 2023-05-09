0185012a4b4062ed90792049cd996d0b54324360
Long <break0344@gmail.com>
2023-05-09 21:26:22 +0800

    comment update test

--- comments/api/views.py
+++ comments/api/views.py
@@ -1,7 +1,4 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from comments.models import Comment
+from comments.api.permissions import IsObjectOwner
 from comments.api.serializers import (
     CommentSerializer,
     CommentSerializerForCreate,
